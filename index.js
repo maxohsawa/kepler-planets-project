b6a6e2cc7b39517f5785081d8aa5f7ddd52ce62a
Max Ohsawa <max.ohsawa@gmail.com>
2022-04-11 02:29:15 -0400

    adds habitability filters: confirmed planet, stellar flux, and planet radius

--- index.js
+++ index.js
@@ -3,17 +3,30 @@ const fs = require('fs');
 
 const results = [];
 
+/*
+  - koi_disposition represents whether the planet has been CONFIRMED
+  - koi_insol represents the amount of stellar emissions that the planet receives as a proportion compared to what Earth receives from Sol. This value should be between 0.36 and 1.11
+  - koi_prad represents the planetary radius as a proportion of Earth's radius. This value should be a maximum of 1.6 as larger planets tend to be gaseous
+*/
+function isHabitablePlanet(planet) {
+  return planet['koi_disposition'] === 'CONFIRMED'
+    && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+    && planet['koi_prad'] < 1.6;
+}
+
 fs.createReadStream('kepler_data.csv')
   .pipe(parse({
     comment: '#',
     columns: true
   }))
   .on('data', (data) => {
-    results.push(data);
+    if (isHabitablePlanet(data)) {
+      results.push(data);
+    }
   })
   .on('end', () => {
     console.log(results);
-    console.log('done');
+    console.log(`${results.length} habitable planets found!`);
   })
   .on('error', (err) => {
     console.log(err);
